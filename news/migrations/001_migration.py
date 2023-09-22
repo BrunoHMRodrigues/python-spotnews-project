27fb41eabdfdbb5cdfc5f35e4bdf994f705b14e6
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-09-22 18:18:10 -0300

    Feature: Requisite 02 - Created model to Users

--- news/migrations/001_migration.py
+++ news/migrations/001_migration.py
@@ -18,7 +18,25 @@ class Migration(migrations.Migration):
                         verbose_name="ID",
                     ),
                 ),
-                ("name", models.CharField(max_length=200, null=False, blank=False)),
+                ("name", models.CharField(max_length=200, null=False)),
+            ],
+        ),
+        migrations.CreateModel(
+            name="Users",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=200, null=False)),
+                ("email", models.EmailField(max_length=200, null=False)),
+                ("password", models.CharField(max_length=200, null=False)),
+                ("role", models.CharField(max_length=200, null=False)),
             ],
         ),
     ]
