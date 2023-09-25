f8c1e8be8079d74eb5a82bc0389350c136e6b8d7
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-09-25 18:40:13 -0300

    Feature: Requisite 09 - Created drf to users

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -20,9 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from news_rest.views.categories_view import CategoriesViewSet
+from news_rest.views.users_view import UsersViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoriesViewSet)
+router.register(r'users', UsersViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
