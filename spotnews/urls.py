cc14d0d9532179aaa739817d27720065448b77ae
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-09-25 18:19:48 -0300

    Feature: Requisite 08 - created drf to categories

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -18,10 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from news_rest.views.categories_view import CategoriesViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoriesViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("news.urls")),
+    path("api/", include(router.urls)),
 ]
 
 if settings.DEBUG:
